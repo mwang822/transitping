0dec227b1af1d67b230093b19d9c99c663c9aacc
michael wang <mwang@michaels-MacBook-Air.local>
2023-05-07 23:36:20 -0400

    added dotenv and webpack; need to have index.js display properly in order next

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const dotEnv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/javascript/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public','dist'),
+  },
+  plugins: [
+    new dotEnv()
+  ]
+};
