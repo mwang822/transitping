636f3cccf2d003db3b0d5d16aa30f2ed620841f7
michael wang <mwang@michaels-MacBook-Air.local>
2023-05-15 14:47:52 -0400

    Routed bustime api through express server

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 const express = require("express");
 const { hostname } = require("os");
+//const fetch = require("node-fetch");
+//const fetch = import("node-fetch");
+//import fetch from 'node-fetch';
 const path = require("path");
 const app = express();
 const port = 5070;
 
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
 //app.use("/static",express.static(path.resolve(__dirname, "frontend", "static")));
 app.use(express.static(path.resolve(__dirname, 'public', 'dist')));
 app.use(express.static(path.resolve(__dirname, 'src', 'javascript')));
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://bustime.mta.info, https://transitping1.onrender.com, http://localhost:5070');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.get('/data/nearby_bus', (req, res) => {
+  const userLat = req.query.lat;
+  console.log(userLat);
+  const userLon = req.query.lon;
+  const url = `http://bustime.mta.info/api/where/stops-for-location.json?lat=${userLat}&lon=${userLon}&latSpan=0.005&longSpan=0.005&key=d7cdf7f2-042c-4657-993f-97823a8bd34d`;
+  // Fetch request inside the Express route handler
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // send response to the client
+      res.send(data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Error fetching remote resource');
+  });
+});
+app.get('/data/bus_stop', (req, res) =>{
+  const stopRef = req.query.MonitoringRef;
+  const url = `https://bustime.mta.info/api/siri/stop-monitoring.json?version=2&StopMonitoringDetailLevel=minimum&MonitoringRef=${req.query.MonitoringRef}&key=d7cdf7f2-042c-4657-993f-97823a8bd34d`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      //send response back to client
+      res.send(data);
+    }).catch(error => {
+      console.log(error);
+      res.status(500).send('Error fetching remote resource');
+    });
+});
 
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname,"public", "dist", "index.html"));
 });
-console.log(process.env.BUS_API_KEY);
+
+
 app.listen(process.env.PORT || port, '0.0.0.0', () => console.log(`Server running on port ${port}...`));
